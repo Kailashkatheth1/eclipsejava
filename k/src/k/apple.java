8d78a1e132af69c1bb09b98a940a8cea3ae9d96e
kailashkatheth1 <kailashakatheth56@gmail.com>
2018-01-15 14:18:06 +0545

    constructor overloading

--- k/src/k/apple.java
+++ k/src/k/apple.java
@@ -4,8 +4,14 @@ public class apple {
 
 	public static void main(String [] args) {
 	tuna tunaObject=new tuna();
-	System.out.println(tunaObject.toMilitary());
-	tunaObject.setTime(13, 27, 6);
-	System.out.println(tunaObject.toMilitary());
+	tuna tunaObject1=new tuna(5);
+	tuna tunaObject2=new tuna(5,13);
+	tuna tunaObject3=new tuna(5,13,43);
+	
+	
+	System.out.printf("%s\n",tunaObject.toMilitary());
+	System.out.printf("%s\n",tunaObject1.toMilitary());
+	System.out.printf("%s\n",tunaObject2.toMilitary());
+	System.out.printf("%s\n",tunaObject3.toMilitary());
 	}
 }
